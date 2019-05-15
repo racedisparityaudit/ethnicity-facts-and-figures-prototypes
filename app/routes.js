cef038ef5171d110bff0a370a5b228683dfa7f59
Frankie Roberto <frankie@frankieroberto.com>
2019-05-15 14:35:20 +0100

    Linking to data sources

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/add-data-source-or-create', function (req, res) {
+  let dataSource = req.session.data['data-source-title']
+
+  if (dataSource == 'add-new') {
+    res.redirect('/add-new-data-source')
+  } else {
+    res.redirect('/measure-linked-to-data-sources')
+  }
+
+})
+
 module.exports = router
